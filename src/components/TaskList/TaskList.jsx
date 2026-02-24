809f72bae9fd9f4b8981e32c09fbb6fbef286297
front_academy <dmitrypetrakevich@gmail.com>
2026-02-24 13:52:05 +0700

    добавли анимацию списка

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import { motion, AnimatePresence } from 'framer-motion'
 import styles from "./TaskList.module.css";
 import TaskItem from "../TaskItem/TaskItem";
 
 const TaskList = ({ tasks, title, onDelete, onToggle  }) => {
   return (
     <div>
-      <h1 style={{ textAlign: "center", marginBottom: '20px'}}>{title}</h1>
+      <h1 className={styles.title} style={{ margin: '20px 0'}}>{title}</h1>
 
       <div className={styles.taskList}>
+        <AnimatePresence>
         {tasks.map((task) => (
-          <TaskItem 
-          key={task.id} 
-          body={task.body} 
-          done={task.done}
-          onDelete={() => onDelete(task.id)}
-          onToggle={() => onToggle(task.id)} 
-          />
+            <motion.li
+              key={task.id} 
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.2 }}
+              style={{ listStyle: 'none', padding: 0, margin: 0 }}
+            >
+            <TaskItem 
+              body={task.body} 
+              done={task.done}
+              onDelete={() => onDelete(task.id)}
+              onToggle={() => onToggle(task.id)} 
+              />
+            </motion.li>
         ))}
+        </AnimatePresence>
       </div>
     </div>
   );
